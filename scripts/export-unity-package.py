334c06799cb27ba3e7f46a5cf7613ec0146ee413
Matias Lavik <matias.lavik@catenda.no>
2022-11-18 21:02:11 +0100

    Package

--- scripts/export-unity-package.py
+++ scripts/export-unity-package.py
@@ -12,12 +12,12 @@ if len(sys.argv) > 1:
     unity_path = str(sys.argv[1])
 else:
     print("ERROR: You need to pass the path to Unity editor executable.")
-    return
+    exit()
 
 nodisplay =  "-nodisplay" in sys.argv
 package_name = 'UnityAsyncTextureImport.unitypackage'
 plugin_folder_name = 'UnityAsyncTextureImport'
-unity_project_dir = 'unity-async-texture-import'
+unity_project_dir = 'unity-async-textureimport'
 
 export_project_path = "tmp-package-export"
 
@@ -34,8 +34,11 @@ for asset in assets:
 
 # Copy binaries
 freeImage_bin_dir = os.path.join(export_project_path, "Assets", plugin_folder_name, "Assets", "Plugins", "FreeImage")
-copy_filedir(os.path.join("binaries", "arm64-v8a", "libFreeImage.so)", os.path.join(freeImage_bin_dir, "Android", "arm64-v8a", "libFreeImage.so"))
-copy_filedir(os.path.join("binaries", "armeabi-v7a", "libFreeImage.so)", os.path.join(freeImage_bin_dir, "Android", "armeabi-v7a", "libFreeImage.so"))
+os.mkdir(os.path.join(freeImage_bin_dir, "Android"))
+os.mkdir(os.path.join(freeImage_bin_dir, "Android", "arm64-v8a"))
+copy_filedir(os.path.join("binaries", "arm64-v8a", "libFreeImage.so"), os.path.join(freeImage_bin_dir, "Android", "arm64-v8a", "libFreeImage.so"))
+os.mkdir(os.path.join(freeImage_bin_dir, "Android", "armeabi-v7a"))
+copy_filedir(os.path.join("binaries", "armeabi-v7a", "libFreeImage.so"), os.path.join(freeImage_bin_dir, "Android", "armeabi-v7a", "libFreeImage.so"))
 
 command_string = "\"{unity_path}\" -projectPath {project_path} -exportPackage Assets {package_name} -batchmode -nographics -silent-crashes -quit".format(unity_path=unity_path, project_path=export_project_path, package_name=package_name)
 # Run through cvfb if no display available (building in container, etc.).
